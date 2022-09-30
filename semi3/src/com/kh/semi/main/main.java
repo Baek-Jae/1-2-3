64541845a623d74bfe4126183ba6c225bdbe0a19
DESKTOP-3MV12EK\cqkf0 <cqkf0102@nate.com>
2022-09-30 17:18:12 +0900

    메인 만들어봅니다
    메인!

---
+++ semi3/src/com/kh/semi/main/main.java
@@ -0,0 +1,10 @@
+package com.kh.semi.main;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("세미프로젝트를 환영합니다.");
+	}
+
+}
