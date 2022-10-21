db28430deb35c8e7acbd96f14156b95f586a3b13
DESKTOP-3MV12EK\cqkf0 <cqkf0102@nate.com>
2022-10-21 14:45:05 +0900

    세번째 머지
    Notice 작업중입니다~!

--- workspace/semiPrjOmJm/src/com/kh/semi/notice/controller/NoticeListController.java
+++ workspace/semiPrjOmJm/src/com/kh/semi/notice/controller/NoticeListController.java
@@ -1,5 +1,59 @@
 package com.kh.semi.notice.controller;
 
-public class NoticeListController {
+import java.io.IOException;
+import java.util.List;
 
-}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.notice.service.NoticeService;
+import com.kh.semi.notice.vo.NoticeVo;
+
+@WebServlet(urlPatterns = "/notice/list")
+public class NoticeListController extends HttpServlet{
+
+	//공지사항 목록 (화면)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 데이터 꺼내기
+		// 데이터 뭉치기
+		
+		// 디비 다녀오기
+		List<NoticeVo> voList = new NoticeService().selectNoticeList();
+		
+		// 화면선택(데이터 담아서)
+		req.setAttribute("voList", voList);
+		req.getRequestDispatcher("/WEB-INF/views/notice/list.jsp").forward(req, resp);
+		
+	}//doGet
+	
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- workspace/semiPrjOmJm/src/com/kh/semi/notice/service/NoticeService.java
+++ workspace/semiPrjOmJm/src/com/kh/semi/notice/service/NoticeService.java
@@ -1,5 +1,94 @@
 package com.kh.semi.notice.service;
 
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.semi.common.JDBCTemplate;
+import com.kh.semi.notice.dao.NoticeDao;
+import com.kh.semi.notice.vo.NoticeVo;
+
 public class NoticeService {
 
-}
+	//공지사항 작성
+	public int write(NoticeVo vo) {
+		
+		//커넥션 준비
+		//SQL
+		//트랜잭션 , 자원반납
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int result = NoticeDao.insertNotice(conn , vo);
+		
+		if(result == 1) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		
+		JDBCTemplate.close(conn);
+		
+		return result;
+		
+	}//write
+	
+	//공지사항 목록 조회
+	public List<NoticeVo> selectNoticeList() {
+		//커넥션 준비
+		//SQL
+		//트랜잭션 처리 , 자원 반납
+		
+		Connection conn = JDBCTemplate.getConnection();
+		
+		List<NoticeVo> voList = NoticeDao.selectNoticeList(conn);
+		
+		JDBCTemplate. close(conn);
+		
+		return voList;
+		
+	}//selectNoticeList
+
+	//상세조회
+	public NoticeVo selectNoticeOne(String no) {
+		
+		//커넥션 준비
+		//SQL (조회수증가 , 상세조회)
+		//트랜잭션 처리 , 자원반납
+		
+		Connection conn = JDBCTemplate.getConnection();
+		NoticeVo vo = null;
+		
+		int result = NoticeDao.increaseHit(conn , no);
+		if(result == 1) {
+			JDBCTemplate.commit(conn);
+			vo = NoticeDao.selectNoticeOne(conn , no);
+		}
+		
+		JDBCTemplate.close(conn);
+		
+		return vo;
+		
+	}//selectNoticeOne
+	
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
